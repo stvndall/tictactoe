25792578007d534eba9277c493a7e77b22e71ab7
Steven Dall <stvndall@gmail.com>
2023-11-28 20:06:09 +0200

    very basic layout of website

---
+++ web/tests/integration/components/board-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'web/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | board', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Board />`);
+
+    assert.dom().hasText('');
+
+    // Template block usage:
+    await render(hbs`
+      <Board>
+        template block text
+      </Board>
+    `);
+
+    assert.dom().hasText('template block text');
+  });
+});
